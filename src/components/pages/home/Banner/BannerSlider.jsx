7c591841029b23b2ea56064e05127e0eb029009d
AmrFathyMohamed <govany18790@gmail.com>
2025-06-18 13:14:09 +0300

    Azir V1.0

---
+++ src/components/pages/home/Banner/BannerSlider.jsx
@@ -0,0 +1,99 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, A11y, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "swiper/css/autoplay";
+import { ArrowRight } from "@phosphor-icons/react";
+import "./Banner.scss";
+import { Images } from "../../../../assets/images";
+import { Link } from "react-router-dom";
+
+const BannerSlider = () => {
+  return (
+    <div className="comp-banner-slider">
+      <Swiper
+        slidesPerView={1}
+        modules={[Pagination, A11y, Autoplay]}
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 5000, disableOnInteraction: false }}
+      >
+        <SwiperSlide>
+          <div className="banner-item">
+            <div className="banner-item-l">
+              <p className="banner-item-text-t">the best place to play</p>
+              <h3 className="banner-item-ttl">Xbox Consoles</h3>
+              <div className="banner-item-text">
+                Save up to 50% on select Xbox games. Get 3 months of PC Game
+                Pass for $2 USD.
+              </div>
+              <Link to="/shop" className="btn btn-primary">
+                <span className="btn-text">shop now</span>
+                <span className="btn-icon">
+                  <ArrowRight size={24} />
+                </span>
+              </Link>
+            </div>
+            <div className="banner-item-r">
+              <div className="banner-item-img">
+                <img src={Images.Banner1} alt="" />
+                <div className="banner-item-tag price">$299</div>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="banner-item">
+            <div className="banner-item-l">
+              <p className="banner-item-text-t">the best place to play</p>
+              <h3 className="banner-item-ttl">Xbox Consoles</h3>
+              <div className="banner-item-text">
+                Save up to 50% on select Xbox games. Get 3 months of PC Game
+                Pass for $2 USD.
+              </div>
+              <Link to="/shop" className="btn btn-primary">
+                <span className="btn-text">shop now</span>
+                <span className="btn-icon">
+                  <ArrowRight size={24} />
+                </span>
+              </Link>
+            </div>
+            <div className="banner-item-r">
+              <div className="banner-item-img">
+                <img src={Images.Banner1} alt="" />
+                <div className="banner-item-tag price">$299</div>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="banner-item">
+            <div className="banner-item-l">
+              <p className="banner-item-text-t">the best place to play</p>
+              <h3 className="banner-item-ttl">Xbox Consoles</h3>
+              <div className="banner-item-text">
+                Save up to 50% on select Xbox games. Get 3 months of PC Game
+                Pass for $2 USD.
+              </div>
+              <Link to="/shop" className="btn btn-primary">
+                <span className="btn-text">shop now</span>
+                <span className="btn-icon">
+                  <ArrowRight size={24} />
+                </span>
+              </Link>
+            </div>
+            <div className="banner-item-r">
+              <div className="banner-item-img">
+                <img src={Images.Banner1} alt="" />
+                <div className="banner-item-tag price">$299</div>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default BannerSlider;
---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+// app pages
+export { default as Home } from "./Home/HomeScreen";
+export { default as Shop } from "./Shop/ShopScreen";
+export { default as ProductDetails } from "./ProductDetails/ProductDetailsScreen";
+export { default as Cart } from "./Cart/CartScreen";
+export { default as NotFound } from "./NotFound/NotFoundScreen";
+export { default as TrackOrder } from "./TrackOrder/TrackOrderScreen";
+export { default as TrackOrderDetails } from "./TrackOrder/TrackOrderDetailsScreen";
+export { default as Compare } from "./Compare/CompareScreen";
+export { default as Wishlist } from "./Wishlist/WishlistScreen";
+export { default as Checkout } from "./Checkout/CheckoutScreen";
+export { default as CheckoutSuccess } from "./Checkout/CheckoutSuccessScreen";
+export { default as Support } from "./Support/SupportScreen";
+
+// auth pages
+export { default as Sign } from "./Sign/SignScreen";
+export { default as ForgotPassword } from "./Sign/ForgotPasswordScreen";
+export { default as ResetPassword } from "./Sign/ResetPasswordScreen";
+export { default as Verify } from "./Sign/VerifyScreen";
+export { default as Faq } from "./Faq/FaqScreen";
+
+
